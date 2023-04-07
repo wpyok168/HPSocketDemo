3b102fce33dbd2891322164aa278871b7344ba56
wpyok500 <wpyok500@gmail.com>
2023-04-07 13:06:48 +0800

    添加项目文件。

---
+++ HPSocketDemo/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using GetCIDbyBatchActivation;
+using HPSocket;
+using HPSocket.Http;
+using HPSocket.Tcp;
+
+namespace HPSocketDemo
+{
+    internal class Program
+    {
+        private static IHttpEasyServer hs;
+        private static string html = string.Empty;
+        static void Main(string[] args)
+        {
+            using (hs = new HttpEasyServer())
+            {
+                hs.Port = 8899;
+
+                hs.OnRequestLine += Hs_OnRequestLine;
+
+                if (!hs.Start())
+                {
+                    Console.WriteLine($@"httpServer.Start() error, code：{hs.ErrorCode}, message: {hs.ErrorMessage}");
+                    return;
+                }
+                Console.WriteLine($@"bind: {hs.Address}:{hs.Port}");
+                hs.Wait();
+            }
+            Console.ReadKey();
+        }
+
+        private static HttpParseResult Hs_OnRequestLine(IHttp sender, IntPtr connId, string method, string url)
+        {
+            hs.OnEasyMessageData += Hs_OnEasyMessageData;
+
+            if (url.ToString().Contains("getcid/?iids="))
+            {
+                string cid = string.Empty;
+                string str = System.Web.HttpUtility.UrlDecode(url);
+                string iid = Regex.Replace(str, "-", "").Replace(" ", "").Replace("getcid/?iids=", "");
+                iid = Regex.Match(iid, "[\\d]+").Value;
+                if (Regex.IsMatch(iid, "[\\d]{63}"))
+                {
+                    iid = Regex.Match(iid, "[\\d]{63}").Value;
+                }
+                else if (Regex.IsMatch(iid, "[\\d]{54}"))
+                {
+                    iid = Regex.Match(iid, "[\\d]{54}").Value;
+                }
+                //string iid = "690494024111248275641345743539870015978502549282339542426203524";
+                cid = XmlRequest.MSXmlRequest(1, iid, "00000-04249-038-820384-03-2052-9200.0000-0902023");
+                html = $"安装ID：{iid}</br>确认ID：{cid}";
+                
+            }
+            return HttpParseResult.Ok;
+        }
+
+        private static HttpParseResult Hs_OnEasyMessageData(IHttpEasyServer sender, IntPtr connId, byte[] data)
+        {
+            var headers = sender.GetAllHeaders(connId);
+            var sb = new StringBuilder();
+            foreach (var item in headers)
+            {
+                sb.Append($"{item.Name}: {item.Value}<br />");
+            }
+
+            string a =Encoding.UTF8.GetString(data);
+
+
+
+            // 替换模板中的{{headers}}变量
+            //var html = "<h1>获取确认id</h1>";
+
+            // 响应体
+            var responseBody = Encoding.GetEncoding("gb2312").GetBytes(html);
+            // 响应头
+            var responseHeaders = new List<NameValue>
+            {
+                new NameValue{ Name = "Content-Type", Value = "text/html"},
+            };
+
+            // 发送响应数据到客户端
+            if (!sender.SendResponse(connId, HttpStatusCode.Ok, responseHeaders, responseBody, responseBody.Length))
+            {
+                return HttpParseResult.Error;
+            }
+
+            // 不是保活连接踢掉
+            if (sender.GetHeader(connId, "Connection") != "Keep-Alive")
+            {
+                sender.Release(connId);
+            }
+
+            return HttpParseResult.Ok;
+        }
+    }
+}
